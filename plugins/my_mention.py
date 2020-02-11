2e7114079173835aff3e062babcd0376c9d36745
Radperia <rexalmeria826@gmail.com>
2020-02-11 22:13:34 +0900

    first modules

---
+++ plugins/my_mention.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+from slackbot.bot import default_reply
+import random
+
+@respond_to('次回のゼミ')
+def seminar(message):
+    message.reply('次回のゼミはありません')
+
+
+@respond_to('飯屋')
+def food(message):
+    shop_list = ['我羅奢', '蔭山', '表裏', '中本', 'ピコピコポン', '波風', '破壊的', 'こころ', 'マック']
+    message.reply(shop_list[random.randrange(len(shop_list))])
+
+
+@default_reply()
+def default_func(message):
+    message.reply('いい加減バグ治って欲しい')
