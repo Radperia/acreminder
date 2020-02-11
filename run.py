7c655f9584a93bea6f270b5af48eb44379e84e76
Radperia <rexalmeria826@gmail.com>
2020-02-11 21:59:43 +0900

    nothing

--- run.py
+++ run.py
@@ -43,14 +43,14 @@ def info(channel, slack):
 def seminar(message):
     message.reply('次回のゼミはありません')
 
+@respond_to('飯屋')
+def food(message):
+    shop_list = ['我羅奢', '蔭山', '表裏', '中本', 'ピコピコポン', '波風', '破壊的', 'こころ', 'マック']
+    message.reply(shop_list[random.randrange(len(shop_list))])
+
 @default_reply()
 def default_func(message):
     message.reply('ほげほげ')
-
-@respond_to('飯屋')
-def food(message):
-    shop_list = ['我羅奢', '蔭山', '表裏', '中本', 'ピコピコポン', '波風', '破壊的', 'こころ']
-    message.reply(shop_list[random.randrange(len(shop_list))])
 
 def main():
     #Botを動かす前にチャンネルでのBotアプリケーションの追加を忘れずに
