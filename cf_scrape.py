78db8474e8e9dfe818a6aef2fdb228f4a7a6da50
Radperia <rexalmeria826@gmail.com>
2020-02-15 19:18:17 +0900

    cf v0.1

--- cf_scrape.py
+++ cf_scrape.py
@@ -30,8 +30,9 @@ def scrape_upcoming():
         d1 = contests2[i].find_all("a")
 
         for j in range(len(d1)):
-            print(d1[j])
-            print("---------------------------")
+            #print("here is " + str(j))
+            #print(d1[j])
+            #print("---------------------------")
 
             if(j != 0):
                 time_container = d1[j].find("span")
@@ -46,40 +47,26 @@ def scrape_upcoming():
                     break
 
                 re_contests_sub.append(t.strftime('%Y-%m-%d-%a %H:%M') + " 開始")
-                print(re_contests_sub)
+                #print(re_contests_sub)
 
-        d2 = d1[j].find("a", href = re.compile("contests"))
-        print(d2)
+                #d2 = d1[j].find("a", class_="contestParticipantCountLinkMargin")
+                #print(type(d2))
 
-        re_contests_sub.append(access_url + d2.get("href"))
-        re_contests.append(re_contests_sub)
+                #print("HERE IS D1")
+                #print(d1[j])
+                #if(d2 is None):
+                #    continue
+                d2 = d1[j+1].get("href")
+                #print("HERE IS D2")
+                #print(d2)
+                if(d2 is None):
+                    continue
+                re_contests_sub.append(access_url + d2)
+                re_contests.append(re_contests_sub)
 
     return re_contests
-
-'''
-def dayofweek(date_sub):
-    yobi = ["月", "火", "水", "木", "金", "土", "日"]
-    while(date_sub != "bye"):
-        try:
-            a = datetime.strptime(date_sub, '%Y/%m/%d')
-            return yobi[a.weekday()]
-        except ValueError:
-            print("something went wrong.")
-    else:
-        sys.exit(1)
-'''
 
 def strtotime(date_sub):
 
     return datetime.datetime.strptime(date_sub, 
     '%b/%d/%Y %H:%M')
-
-
-def timetostr(date_sub):
-    #datetimeオブジェクトをstrオブジェクトにして返す
-
-    W = ["月", "火", "水", "木", "金", "土", "日"]
-    return ('%d-%d-%d(%s) %d:%s' % (
-        date_sub.year, date_sub.month, date_sub.day, W[date_sub.weekday(
-        )], date_sub.hour, str(date_sub.minute).ljust(2, "0")
-    ))
