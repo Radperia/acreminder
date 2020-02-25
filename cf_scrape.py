b38bf6597d018f4a16fdf1c870fa7754de197f62
Radperia <rexalmeria826@gmail.com>
2020-02-25 15:42:12 +0900

    changed most of functions

--- cf_scrape.py
+++ cf_scrape.py
@@ -4,69 +4,138 @@ import re
 import datetime
 import sys
 
-def scrape_upcoming():
-    #一週間以内に開催されるコンテストの情報を返す関数
+def scrape_active():
+    #現在開催中のコンテスト情報を返す関数
 
+    contests_list = []
     re_contests = []
+    active_contests_sub = []
 
     url = "https://codeforces.com/contests/"
     access_url = "https://codeforces.com"
     html = request.urlopen(url)
     soup = BeautifulSoup(html, "html.parser")
 
-    contests1 = soup.find("div", class_ = "datatable")
-    contests2 = contests1.find_all("tr")
+    contests1 = soup.find("div" , class_ = "datatable")
+    contests2 = contests1.find_all("td")
 
     w = datetime.datetime.today()
 
-    #print(len(contests2))
+    for i in range(len(contests2)):
+        contest_title = ""
+        start_time = datetime.datetime.today()
+        length = ""
+        contest_url = ""
+
+        #print("--------------------------")
+        #print("Here is " + str(i))
+        #print(re_contests)
+
+        if(i%6==0):
+            contest_title = str(contests2[i].text)
+            contest_title = re.sub('[\r\n]+$', '', contest_title)
+            active_contests_sub.append(contest_title)
+
+        if(i%6==2):
+            start_time = strtotime(contests2[i].span.text)
+            start_time += datetime.timedelta(hours=6)
+            # 現在時刻より開始時刻が後の時return
+            if(w - start_time).days <= 0:
+                # 苦し紛れ
+                del contests_list[-1][-1][-1]
+                return contests_list
+            active_contests_sub.append(timetostr(start_time) + " 開始")
+
+        if(i%6==3):
+            length = contests2[i].text
+            active_contests_sub.append(length)
+
+        if(i%6==5):
+            try:
+                contest_url = contests2[i].a.get("href")
+                active_contests_sub.append(access_url + contest_url)
+            except AttributeError:
+                pass
+
+        if(i%6==5):
+            re_contests.append(active_contests_sub)
+            contests_list.append(re_contests)
+
+def scrape_upcoming():
+    #1週間以内に開催されるコンテスト情報を返す関数
+
+    contests_list = []
+    re_contests = []
+    active_contests_sub = []
+
+    url = "https://codeforces.com/contests/"
+    access_url = "https://codeforces.com"
+    html = request.urlopen(url)
+    soup = BeautifulSoup(html, "html.parser")
+
+    contests1 = soup.find("div" , class_ = "datatable")
+    contests2 = contests1.find_all("td")
+
+    w = datetime.datetime.today()
+
+    before_flag = False
 
     for i in range(len(contests2)):
-        re_contests_sub = []
+        contest_title = ""
+        start_time = datetime.datetime.today()
+        length = ""
+        contest_url = ""
 
-        if(i == 0):
-            continue
+        if(i%6==0):
+            if(before_flag is False):
+                contest_title = str(contests2[i].text)
+                contest_title = re.sub('[\r\n]+$', '', contest_title)
+                active_contests_sub.append(contest_title)
 
-        d1 = contests2[i].find_all("a")
+        if(i%6==2):
+            start_time = strtotime(contests2[i].span.text)
+            start_time += datetime.timedelta(hours=6)
+            if(start_time - w).days < 0:
+                before_flag = True
+                active_contests_sub.clear()
 
-        for j in range(len(d1)):
-            #print("here is " + str(j))
-            #print(d1[j])
-            #print("---------------------------")
+            elif(start_time - w).days > 7:
+                # 苦し紛れ
+                del re_contests[-1]
+                return re_contests
 
-            if(j != 0):
-                time_container = d1[j].find("span")
-                if(time_container is None):
-                    continue
+            else:
+                before_flag = False
+                active_contests_sub.append(timetostr(start_time) + " 開始")
 
-                # tがコンテスト開催時刻を保有
-                t = strtotime(time_container.text)
-                t += datetime.timedelta(hours=6)
+        if(i%6==3):
+            if(before_flag is False):
+                length = contests2[i].text
+                active_contests_sub.append("コンテスト時間 " + length)
 
-                if(t - w).days >= 7:
-                    break
+        if(i%6==5):
+            if(before_flag is False):
+                try:
+                    contest_url = contests2[i].a.get("href")
+                    active_contests_sub.append(access_url + contest_url)
+                except AttributeError:
+                    pass
 
-                re_contests_sub.append(t.strftime('%Y-%m-%d-%a %H:%M') + " 開始")
-                #print(re_contests_sub)
-
-                #d2 = d1[j].find("a", class_="contestParticipantCountLinkMargin")
-                #print(type(d2))
-
-                #print("HERE IS D1")
-                #print(d1[j])
-                #if(d2 is None):
-                #    continue
-                d2 = d1[j+1].get("href")
-                #print("HERE IS D2")
-                #print(d2)
-                if(d2 is None):
-                    continue
-                re_contests_sub.append(access_url + d2)
-                re_contests.append(re_contests_sub)
+        if(i%6==5):
+            if(before_flag is False):
+                re_contests.append(active_contests_sub)
 
     return re_contests
 
 def strtotime(date_sub):
 
-    return datetime.datetime.strptime(date_sub, 
-    '%b/%d/%Y %H:%M')
+    return datetime.datetime.strptime(date_sub, '%b/%d/%Y %H:%M')
+
+def timetostr(date_sub):
+    #datetimeオブジェクトをstrオブジェクトにして返す
+
+    W = ["月", "火", "水", "木", "金", "土", "日"]
+    return ('%d-%d-%d(%s) %d:%s' % (
+        date_sub.year, date_sub.month, date_sub.day, W[date_sub.weekday()],
+        date_sub.hour, str(date_sub.minute).ljust(2, "0")
+    ))
